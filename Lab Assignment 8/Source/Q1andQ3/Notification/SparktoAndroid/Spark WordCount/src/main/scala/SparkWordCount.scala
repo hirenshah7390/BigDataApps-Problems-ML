946661b9eb2665ba33082db73e89431cfacb8f87
Hiren Shah <hirenshah7390@hotmail.com>
2016-03-17 21:52:25 -0500

    commit

---
+++ Lab Assignment 8/Source/Q1andQ3/Notification/SparktoAndroid/Spark WordCount/src/main/scala/SparkWordCount.scala
@@ -0,0 +1,36 @@
+
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * Created by Mayanka on 09-Sep-15.
+ */
+object SparkWordCount {
+
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir","c:\\winutils");
+
+    val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+
+    val sc=new SparkContext(sparkConf)
+
+    val input=sc.textFile("C:\\Users\\hhstm4\\Desktop\\Spring2016\\input.txt")
+
+    val wc=input.flatMap(line=>{line.split(" ")}).map(word=>(word,1)).cache()
+
+    val output=wc.reduceByKey(_+_)
+
+//    output.saveAsTextFile("output")
+
+    val o=output.collect()
+
+    var s:String="Words:Count \n"
+    o.foreach{case(word,count)=>{
+      s+=word+" : "+count+"\n"
+    }}
+
+    SocketClient.sendCommandToRobot(s)
+  }
+
+}
